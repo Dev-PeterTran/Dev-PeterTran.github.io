633301919c5d3a75965633dca23979f1a5b8e07b
Peter Tran <dev.petertran@gmail.com>
2025-05-01 16:01:49 -0400

    Navbar scroll

--- Scripts/Navbar.js
+++ Scripts/Navbar.js
@@ -11,3 +11,38 @@ function ToggleDropdown(event) {
 		dropdown.classList.toggle('active-dropdown');
 	}
 }
+window.addEventListener('load', () => {
+    const hash = window.location.hash;
+    if (hash) {
+        const target = document.querySelector(hash);
+        if (target) {
+            target.scrollIntoView({ behavior: 'smooth' });
+        }
+    }
+});
+
+
+const sections = document.querySelectorAll("div[id]");
+const navLinks = document.querySelectorAll(".NavItems li a");
+
+const observer = new IntersectionObserver(
+    entries => {
+        entries.forEach(entry => {
+            const id = entry.target.getAttribute("id");
+            const navLink = document.querySelector(`.NavItems li a[href="#${id}"]`);
+            if (navLink) {
+                if (entry.isIntersecting) {
+                    navLinks.forEach(link => link.classList.remove("active"));
+                    navLink.classList.add("active");
+                }
+            }
+        });
+    },
+    {
+        threshold: 0.6,
+    }
+);
+
+sections.forEach(section => {
+    observer.observe(section);
+});
