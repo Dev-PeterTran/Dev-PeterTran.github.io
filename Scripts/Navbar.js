1a2d5409c41f3884c2c6a77822631d8c322b653f
Peter Tran <dev.petertran@gmail.com>
2025-05-07 18:15:09 -0400

    Improved webpage style

--- Scripts/Navbar.js
+++ Scripts/Navbar.js
@@ -63,3 +63,35 @@ function GoBack() {
 		window.location.href = "../index.html#Projects";
 	}
 }
+
+// Typing text effect beside my name
+const roles = ["Game Developer", "Software Developer"];
+let i = 0;
+let j = 0;
+let currentRole = "";
+let isDeleting = false;
+const typingElement = document.getElementById("TypingText");
+
+function type() {
+	if (i >= roles.length) i = 0;
+	currentRole = roles[i];
+
+	typingElement.innerHTML = currentRole.substring(0, j) + (j % 2 === 0 ? "|" : "");
+
+	if (!isDeleting && j < currentRole.length) {
+		j++;
+		setTimeout(type, 150); // slower typing
+	} else if (isDeleting && j > 0) {
+		j--;
+		setTimeout(type, 100); // slower deleting
+	} else if (!isDeleting && j === currentRole.length) {
+		isDeleting = true;
+		setTimeout(type, 1500); // pause before delete
+	} else if (isDeleting && j === 0) {
+		isDeleting = false;
+		i++;
+		setTimeout(type, 500); // pause before next word
+	}
+}
+
+type();
